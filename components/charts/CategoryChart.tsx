3cfd0ff60cad619d55521884a39ca40d507b525d
geekysam7 <geekysam7@gmail.com>
2024-01-07 17:33:12 +0530

    add filter support based on x/y axis in category chart

--- components/charts/CategoryChart.tsx
+++ components/charts/CategoryChart.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import {
   BarChart,
   Bar,
@@ -11,13 +11,71 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import _isEmpty from "lodash/isEmpty";
+import _filter from "lodash/filter";
 
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { TCategoryAggregation } from "@/types/app";
 import {
   dataFormatter,
   parseInstallSize,
   tickFormatter,
 } from "@/helpers/general";
+
+const cagtegoryAggregationData = [
+  {
+    label: "Category",
+    value: "name",
+  },
+  {
+    label: "Reviews",
+    value: "reviews",
+  },
+  ,
+  {
+    label: "Installs",
+    value: "installs",
+  },
+] as { label: string; value: string }[];
+
+export function AxisSelect({
+  data,
+  onChange,
+  axisLabel,
+  selected,
+}: {
+  data: { value: string; label: string }[];
+  onChange: (value: string) => void;
+  axisLabel: string;
+  selected: string;
+}) {
+  return (
+    <Select onValueChange={onChange} value={selected}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder={`Select ${axisLabel} key`} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>{axisLabel}</SelectLabel>
+          {data.map(({ value, label }) => {
+            return (
+              <SelectItem key={value} value={value}>
+                {label}
+              </SelectItem>
+            );
+          })}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
 
 const CustomTooltip = ({
   active,
@@ -69,32 +127,65 @@ const CategoryAggregationChart = ({
 }: {
   data: TCategoryAggregation[];
 }) => {
+  const [xAxis, setXAxis] = useState(cagtegoryAggregationData[0].value);
+  const [yAxis, setYAxis] = useState(cagtegoryAggregationData[1].value);
+  const handleXAxisChange = (value: string) => {
+    setXAxis(value);
+  };
+  const handleYAxisChange = (value: string) => {
+    setYAxis(value);
+  };
+
+  const xAxisData = useMemo(() => {
+    return cagtegoryAggregationData.filter(({ value }) => value !== yAxis);
+  }, [yAxis]);
+
+  const yAxisData = useMemo(() => {
+    return cagtegoryAggregationData.filter(({ value }) => value !== xAxis);
+  }, [xAxis]);
+
   return (
-    <ResponsiveContainer width="100%" height={450}>
-      <BarChart data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis
-          dataKey="name"
-          stroke="#888888"
-          fontSize={10}
-          angle={-45}
-          textAnchor="end"
-          height={100}
+    <>
+      <div className="p-4 flex gap-4">
+        <AxisSelect
+          data={xAxisData}
+          onChange={handleXAxisChange}
+          axisLabel="x axis"
+          selected={xAxis}
         />
-        <YAxis tickFormatter={tickFormatter} stroke="#888888" fontSize={12} />
-        {/* @ts-ignore */}
-        <Tooltip content={<CustomTooltip />} />
-        <Legend />
-        <Bar
-          dataKey="reviews"
-          fill="currentColor"
-          className="fill-primary"
-          radius={[4, 4, 0, 0]}
-          activeBar={<Rectangle fill="gold" stroke="purple" />}
-          background={{ fill: "#eee" }}
+        <AxisSelect
+          data={yAxisData}
+          onChange={handleYAxisChange}
+          axisLabel="y axis"
+          selected={yAxis}
         />
-      </BarChart>
-    </ResponsiveContainer>
+      </div>
+      <ResponsiveContainer width="100%" height={450}>
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey={xAxis}
+            stroke="#888888"
+            fontSize={10}
+            angle={-45}
+            textAnchor="end"
+            height={100}
+          />
+          <YAxis tickFormatter={tickFormatter} stroke="#888888" fontSize={12} />
+          {/* @ts-ignore */}
+          <Tooltip content={<CustomTooltip />} />
+          <Legend />
+          <Bar
+            dataKey={yAxis}
+            fill="currentColor"
+            className="fill-primary"
+            radius={[4, 4, 0, 0]}
+            activeBar={<Rectangle fill="gold" stroke="purple" />}
+            background={{ fill: "#eee" }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
   );
 };
 
